67d315cb725ebcf20e251ef0647b067e33d8e80a
seazyns <yashpatolia1@gmail.com>
2024-11-03 21:06:50 -0500

    fix

--- cogs/commands/link.py
+++ cogs/commands/link.py
@@ -24,9 +24,9 @@ class Link(commands.Cog):
             username = data['profiles'][profile]['data']['display_name']
             uuid = get_uuid(ign)
 
-            if discord_name != interaction.user.global_name:
+            if discord_name != interaction.user.name:
                 print(discord_name)
-                print(interaction.user.global_name)
+                print(interaction.user.name)
                 embed = discord.Embed(
                     colour=discord.Colour.green(),
                     description=f"Your discord in-game is not linked correctly.")
