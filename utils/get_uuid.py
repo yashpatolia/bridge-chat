2fb67d11871b7a0382381025500599ecd272e455
seazyns <yashpatolia1@gmail.com>
2024-11-03 20:39:03 -0500

    fix

--- utils/get_uuid.py
+++ utils/get_uuid.py
@@ -6,7 +6,7 @@ import sqlite3
 def get_uuid(username):
     with sqlite3.connect("temporals.db") as connection:
         cursor = connection.cursor()
-        cursor.execute("SELECT uuid FROM users WHERE ign = ?", (username,))
+        cursor.execute("SELECT uuid FROM users WHERE ign = ?", (username.lower(),))
         uuid = cursor.fetchone()
 
         if uuid:
