b12203a7c6f247a904168d4620fa605939095f82
seazyns <yashpatolia1@gmail.com>
2024-11-18 02:49:48 -0500

    fix #3

--- cogs/errors/error_handling.py
+++ cogs/errors/error_handling.py
@@ -6,8 +6,13 @@ class ErrorHandling(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    def cog_load(self):  # attaching the handler when the cog is loaded and storing the old handler
+        tree = self.client.tree
+        self._old_tree_error = tree.on_error
+        tree.on_error = self.tree_on_error
+
     @commands.Cog.listener()
-    async def on_app_command_error(self, interaction: discord.Interaction, error):
+    async def tree_on_error(self, interaction: discord.Interaction, error):
         if isinstance(error, app_commands.MissingRole):
             await interaction.response.send_message("You don't have the required role.", ephemeral=True)
 
